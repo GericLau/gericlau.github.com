85b76876dabc07c973971542b82b77c2299cb861
Geric Lau <guoxianl@gmail.com>
2013-01-18 17:36:52 +0800

    demo 1

---
+++ demos/menu_demos/js/script.js
@@ -0,0 +1,24 @@
+(function($) {
+  $.fn.collapsable = function(options) {
+    // iterate and reformat each matched element
+    return this.each(function() {
+      // cache this:
+      var obj = $(this);
+      var tree = obj.next('.navigation');
+      obj.click(function(){
+        if( obj.is(':visible') ){tree.toggle();}
+      });
+      $(window).resize(function(){
+        if ( $(window).width() <= 570 ){tree.attr('style','');};
+      });
+    });
+  };
+})(jQuery);
+$(document).ready(function(){
+    $('.slide-trigger').collapsable();
+
+    $('.dropdown-toggle').click(function(e){
+      e.preventDefault();
+      $(this).parent().toggleClass('open');
+    });
+});
